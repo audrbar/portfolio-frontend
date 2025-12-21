9a18f2e552b2255aaf231513a48303a922773a9f
audrbar <audrbar@gmail.com>
2025-12-21 19:17:11 +0200

    Date made dynamic

--- src/wrapper/AppWrap.js
+++ src/wrapper/AppWrap.js
@@ -9,7 +9,7 @@ const AppWrap = (Component, idName, classNames) => function HOC() {
         <Component />
 
         <div className="copyright">
-          <p className="p-text">@2022 AUDRIUS</p>
+          <p className="p-text">@{new Date().getFullYear()} AUDRIUS</p>
           <p className="p-text">All rights reserved</p>
         </div>
       </div>
